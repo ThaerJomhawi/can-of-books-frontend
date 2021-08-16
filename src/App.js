6870d88a6a86724eaa3731e2a44d4b0db3ba667e
ThaerJomhawi <thaer.fjomhawi@gmail.com>
2021-08-16 15:22:34 +0300

    finsh Lab11

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
-import React from 'react';
-import Header from './Header';
-import IsLoadingAndError from './IsLoadingAndError';
-import Footer from './Footer';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Profile from './Profile';
-import BestBooks from './BestBooks';
-import { withAuth0 } from '@auth0/auth0-react';
-import LoginButton from './LoginButton';
-
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Profile from "./Profile";
+import BestBooks from "./BestBooks";
+import { withAuth0 } from "@auth0/auth0-react";
+import Login from "./Login";
 
 class App extends React.Component {
-
   render() {
-    console.log('app', this.props);
-    return(
+    console.log("app", this.props);
+    return (
       <>
-      
         <Router>
-          <IsLoadingAndError>
-            <Header />
-            <Switch>
-              <Route exact path="/">
-                {this.props.auth0.isAuthenticated ? <BestBooks/> : <LoginButton/>}
-                
-              </Route >
-              <Route path='/profile'>
-                <Profile/>
-              </Route>
-              {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
-            </Switch>
-            <Footer />
-          </IsLoadingAndError>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              {this.props.auth0.isAuthenticated ? <BestBooks /> : <Login />}
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+          </Switch>
+          <Footer />
         </Router>
-        
       </>
     );
   }
